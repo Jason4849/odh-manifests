d778348bf3c9a2cd421f25849f203f5aad0cd760
Jooho Lee <ljhiyh@gmail.com>
2021-12-02 15:26:29 -0500

    update

--- tests/resources/pachyderm/pachyderm-secret-create.sh
+++ tests/resources/pachyderm/pachyderm-secret-create.sh
@@ -2,6 +2,8 @@ export id=$(oc get secret ceph-nano-credentials -o jsonpath='{ .data.AWS_ACCESS_
 export secret=$(oc get secret ceph-nano-credentials -o jsonpath='{ .data.AWS_SECRET_ACCESS_KEY}'|base64 -d)
 export endpoint=$(oc get svc ceph-nano-0 -o jsonpath='{ .spec.clusterIP }' )
 
+oc delete secret pachydem-ceph-secret --ignore-not-found
+
 oc create secret generic pachyderm-ceph-secret \
 --from-literal=access-id=${id}  \
 --from-literal=access-secret=${secret} \

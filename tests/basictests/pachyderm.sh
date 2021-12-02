b7c5de279d2fe7299cb0928e74ca3752976b8d0b
Jooho Lee <ljhiyh@gmail.com>
2021-12-02 14:35:39 -0500

    pachyderm test

--- tests/basictests/pachyderm.sh
+++ tests/basictests/pachyderm.sh
@@ -5,39 +5,47 @@ source $TEST_DIR/common
 MY_DIR=$(readlink -f `dirname "${BASH_SOURCE[0]}"`)
 
 source ${MY_DIR}/../util
-PACHYDERM_CR="${MY_DIR}/../resources/pachyderm-cr.yaml"
+PACHYDERM_CR="${MY_DIR}/../resources/pachyderm/pachyderm-cr.yaml"
+PACHYDERM_SECRET="${MY_DIR}/../resources/pachyderm/pachyderm-secret-create.sh"
+PACHYDERM_RESOURCE_DIR="${MY_DIR}/../resources/pachyderm"
 
 
 os::test::junit::declare_suite_start "$MY_SCRIPT"
 
 function create_ceph_bucket() {
-    os::cmd::expect_success "oc create -f ${HELLOWORD_CR}"
-    header "The hello world example should complete a hello-world workflow with success"
-    os::cmd::try_until_text 'oc get workflow -o jsonpath="{$.items[*].status.phase}"' "Succeeded"
+    header "Create a nes Ceph bucket `my-new-bucket`"
+    os::cmd::expect_success "oc rsync ${PACHYDERM_RESOURCE_DIR} ceph-nano-0:/tmp"
+    os::cmd::expect_success "oc exec pod/ceph-nano-0 -- /bin/bash -c /tmp/ceph-bucket-create.sh"
 }
 
 function create_pachyderm_secret() {
-   
+    header "Create a Secret for Pachyderm"
+    os::cmd::expect_success "/bin/bash -c ${PACHYDERM_SECRET}"
 }
 
 function create_pachyderm_cr() {
+    header "Create a Pachyderm CR"
     os::cmd::expect_success "oc create -f ${PACHYDERM_CR}"
-    header "The Pachyderm CR is created"
 }
 
 function test_pachyderm() {
-    header "Testing ODH Argo installation"
+    header "Setup Pachyderm"
     os::cmd::expect_success "oc project ${ODHPROJECT}"
-    os::cmd::try_until_text "oc get deployment argo-server" "argo-server" $odhdefaulttimeout $odhdefaultinterval
-    os::cmd::try_until_text "oc get pods -l app=argo-server --field-selector='status.phase=Running' -o jsonpath='{$.items[*].metadata.name}'" "argo-server" $odhdefaulttimeout $odhdefaultinterval
-    runningpods=($(oc get pods -l app=argo-server --field-selector="status.phase=Running" -o jsonpath="{$.items[*].metadata.name}"))
-    os::cmd::expect_success_and_text "echo ${#runningpods[@]}" "1"
-    os::cmd::try_until_text "oc get pods -l app=workflow-controller --field-selector='status.phase=Running' -o jsonpath='{$.items[*].metadata.name}'" "workflow-controller" $odhdefaulttimeout $odhdefaultinterval
-    runningpods=($(oc get pods -l app=workflow-controller --field-selector="status.phase=Running" -o jsonpath="{$.items[*].metadata.name}"))
-    os::cmd::expect_success_and_text "echo ${#runningpods[@]}" "1"
-    run_helloworld
+
+    create_ceph_bucket
+	create_pachyderm_secret
+	create_pachyderm_cr
+
+    header "Verify Pachyderm"
+    os::cmd::try_until_text "oc get deployment pachd" "pachd" $odhdefaulttimeout $odhdefaultinterval
+    os::cmd::try_until_text "oc get deployment pg-bouncer" "pg-bouncer" $odhdefaulttimeout $odhdefaultinterval
+    os::cmd::try_until_text "oc get statefulset etcd" "etcd" $odhdefaulttimeout $odhdefaultinterval
+    os::cmd::try_until_text "oc get statefulset postgres" "postgres" $odhdefaulttimeout $odhdefaultinterval
+
+    runningpods=($(oc get pods -l suite=pachyderm --field-selector="status.phase=Running" -o jsonpath="{$.items[*].metadata.name}"))
+    os::cmd::expect_success_and_text "echo ${#runningpods[@]}" "3"
 }
 
-test_odhargo
+test_pachyderm
 
 os::test::junit::declare_suite_end

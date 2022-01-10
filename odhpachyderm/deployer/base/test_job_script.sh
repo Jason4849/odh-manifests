4b4bb47539f9ac29c20c4c41c574154e84c59499
Jooho Lee <ljhiyh@gmail.com>
2022-01-10 15:41:56 -0500

    upgrade job.yaml script

--- odhpachyderm/deployer/base/test_job_script.sh
+++ odhpachyderm/deployer/base/test_job_script.sh
@@ -6,10 +6,20 @@
 # However, the storage_secret was set different name, it execute only:
 #  - Pachyderm part
 
+current_prj_name=${PROJECT_NAME}
+operator_prj_name=openshift-operators
+
 isPodReady(){
   local pod_name=$1
-  ready_pod=$(oc get pod $pod_name  --no-headers|awk '{print $2}' |cut -d/ -f1)
-  desired_pod=$(oc get pod $pod_name  --no-headers|awk '{print $2}' |cut -d/ -f2)
+  local prj_name=$2
+
+  if [[ z${prj_name} == z ]]
+  then 
+    prj_name=${current_prj_name}
+  fi
+
+  ready_pod=$(oc get pod -n ${prj_name} |grep $pod_name  |awk '{print $2}' |cut -d/ -f1)
+  desired_pod=$(oc get pod -n ${prj_name} | grep $pod_name |awk '{print $2}' |cut -d/ -f2)
 
   if [[ ${ready_pod} == ${desired_pod} ]]
   then 
@@ -17,23 +27,24 @@ isPodReady(){
   else
     echo "1" # NOT READY
   fi
+  sleep 5
 }
 
 
 # Storage Setup Part
 ## Check if customer set a custom secret name
-pachyderm-ceph-secret=pachyderm-ceph-secret
-if [[ ${pachyderm-ceph-secret} == ${STORAGE_SECRET_NAME} ]]
+pachyderm_secret_name=pachyderm-ceph-secret
+if [[ ${pachyderm_secret_name} == ${STORAGE_SECRET_NAME} ]]
 then
   ## Ceph exist in the same namespace
-  ceph_statefulset_name=$(oc get statefulset --no-headers -o=custom-columns=name:metadata.name)
+  ceph_statefulset_name=$(oc get statefulset -n ${current_prj_name} --no-headers -o=custom-columns=name:metadata.name)
   if [[ z ==  z${ceph_statefulset_name} ]] 
   then
     echo "Ceph is not created. Please add Ceph component into KfDef"
     exit 1
   fi
   
-  ceph_pod_name=$(oc get pod -l app=ceph --no-headers -o=custom-columns=name:metadata.name)
+  ceph_pod_name=$(oc get pod -n ${current_prj_name}  -l app=ceph --no-headers -o=custom-columns=name:metadata.name)
 
   ## Wait for Ceph pod is Ready
   cephReady=1
@@ -42,7 +53,6 @@ then
     echo ""
     echo "INFO: Ceph Nano is NOT Ready."
 
-    sleep 1
     isPodReady ${ceph_pod_name}
     cephReady=$?
 
@@ -54,8 +64,8 @@ then
   echo "Ceph is ready"
 
   ## Create an object bucket "pachyderm-storage"
-  export access_key=$(oc get secret ceph-nano-credentials -o jsonpath='{ .data.AWS_ACCESS_KEY_ID}'|base64 -d)
-  export secret_key=$(oc get secret ceph-nano-credentials -o jsonpath='{ .data.AWS_SECRET_ACCESS_KEY}'|base64 -d)
+  export access_key=$(oc get secret ceph-nano-credentials -n ${current_prj_name} -o jsonpath='{ .data.AWS_ACCESS_KEY_ID}'|base64 -d)
+  export secret_key=$(oc get secret ceph-nano-credentials -n ${current_prj_name}  -o jsonpath='{ .data.AWS_SECRET_ACCESS_KEY}'|base64 -d)
 
   mkdir /tmp/s3
 cat << EOF > /tmp/s3/createBucket.py
@@ -79,22 +89,22 @@ for bucket in conn.get_all_buckets():
 EOF
 
   oc rsync /tmp/s3 ceph-nano-0:/home/
-  oc exec ceph-nano-0 -c ceph-nano -- /usr/bin/yum install python-boto -y
-  oc exec ceph-nano-0 -c ceph-nano -- /usr/bin/python /home/s3/createBucket.py
+  oc exec ceph-nano-0 -n ${current_prj_name}  -c ceph-nano -- /usr/bin/yum install python-boto -y
+  oc exec ceph-nano-0 -n ${current_prj_name}  -c ceph-nano -- /usr/bin/python /home/s3/createBucket.py
   sleep 3
                 
 
   ## Create a secret "pachyderm-ceph-secret" for the bucket "pachderm-storage"
   endpoint=$(oc get svc ceph-nano-0 -o jsonpath='{ .spec.clusterIP }')
 
-  oc create secret generic ${pachyderm_secret_name} \
+  oc create secret generic ${pachyderm_secret_name} -n ${current_prj_name}  \
   --from-literal=access-id=${access_key}  \
   --from-literal=access-secret=${secret_key} \
   --from-literal=custom-endpoint=http://${endpoint} \
   --from-literal=region=us-east-2 \
   --from-literal=bucket=pachyderm-storage
 else 
-  secreat_exist=$(oc get secret ${STORAGE_SECRET_NAME} --no-headers |wc -l)
+  secreat_exist=$(oc get secret ${STORAGE_SECRET_NAME} -n ${current_prj_name}  --no-headers |wc -l)
   if [[ ${secret_exist} == 1 ]]
   then
     pachyderm_secret_name=${STORAGE_SECRET_NAME}
@@ -106,22 +116,38 @@ else
 fi
 # Pachyderm Part
 ## Create Pachyderm CR with the secret
-echo "
-kind: Pachyderm
-apiVersion: aiml.pachyderm.com/v1beta1
-metadata:
-  name: pachyderm-ceph
-spec:
-  console:
-    disable: true
-  pachd:
-    metrics:
-      disable: false
-    storage:
-      amazon:
-        credentialSecretName: ${pachyderm_secret_name}
-      backend: AMAZON
-"|oc apply -f -
+pachydermCRCreated=1
+while [[ ${pachydermCRCreated} != 0 ]]
+do
+  echo "
+  kind: Pachyderm
+  apiVersion: aiml.pachyderm.com/v1beta1
+  metadata:
+    name: odh-pachyderm
+  spec:
+    console:
+      disable: true
+    pachd:
+      metrics:
+        disable: false
+      storage:
+        amazon:
+          credentialSecretName: ${pachyderm_secret_name}
+        backend: AMAZON
+  "|oc apply -n ${current_prj_name} -f -
+
+  if [[ $? != 0 ]]
+  then
+    echo "INFO: Pachyderm CR failed to be created."
+    echo "INFO: Retry to create a Pachyderm CR."
+    pachydermCRCreated=1
+  else
+    echo "INFO: Pachyderm CR is created successfully."
+    pachydermCRCreated=0
+  fi    
+  sleep 5
+done
+
 echo "Pachyderm Deployer is successfully finished"
 
 
